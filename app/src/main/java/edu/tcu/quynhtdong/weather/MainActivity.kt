93274abaed87e732376b04f41ca1f0edf3f020e9
quynhtdong <quynh.t.dong@tcu.edu>
2022-12-06 16:56:31 -0600

    only need external setup

--- app/src/main/java/edu/tcu/quynhtdong/weather/MainActivity.kt
+++ app/src/main/java/edu/tcu/quynhtdong/weather/MainActivity.kt
@@ -8,6 +8,7 @@ import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
@@ -26,6 +27,10 @@ import kotlinx.coroutines.withContext
 import okhttp3.Dispatcher
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -53,8 +58,7 @@ class MainActivity : AppCompatActivity() {
         //
         binding = ActivityMainBinding.inflate(layoutInflater)
         view = binding.root
-        binding.windCv
-        setContentView(R.layout.activity_main)
+        setContentView(view)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
@@ -169,7 +173,39 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayWeather() {
         dialog.dismiss()
+        binding.connectionTv.text = "Updated Just Now"
         binding.cityTv.text = weatherResponse.name
+        binding.temperatureTv.text = getString(R.string.temperature, weatherResponse.main.temp)
+        var desc = weatherResponse.weather[0].description.split(" ")
+        var description = ""
+        for(ch in desc) {
+            description += ch.replaceFirstChar { c -> c.uppercase() } + " "
+        }
+        binding.descriptionTv.text = getString(R.string.description, description, weatherResponse.main.temp_max, weatherResponse.main.temp_min)
+        binding.windDataTv.text = getString(R.string.wind_data, weatherResponse.wind.speed, weatherResponse.wind.deg, weatherResponse.wind.gust)
+        binding.precipitationDataTv.text = getString(R.string.precipitation_data, weatherResponse.main.humidity, weatherResponse.clouds.all)
+
+        when(weatherResponse.weather[0].icon) {
+            "01d" -> binding.conditionIv.setImageResource(R.drawable.ic_01d)
+            "01n" -> binding.conditionIv.setImageResource(R.drawable.ic_01n)
+            "02d" -> binding.conditionIv.setImageResource(R.drawable.ic_02d)
+            "02n" -> binding.conditionIv.setImageResource(R.drawable.ic_02n)
+            "03d", "03n" -> binding.conditionIv.setImageResource(R.drawable.ic_03)
+            "04d", "04n" -> binding.conditionIv.setImageResource(R.drawable.ic_04)
+            "09d", "09n" -> binding.conditionIv.setImageResource(R.drawable.ic_09)
+            "10d" -> binding.conditionIv.setImageResource(R.drawable.ic_10d)
+            "10n" -> binding.conditionIv.setImageResource(R.drawable.ic_10n)
+            "11d", "11n" -> binding.conditionIv.setImageResource(R.drawable.ic_11)
+            "13d", "13n" -> binding.conditionIv.setImageResource(R.drawable.ic_13)
+            "50d", "50n" -> binding.conditionIv.setImageResource(R.drawable.ic_50)
+
+        }
+        binding.otherDataTv.text = getString(R.string.other_data, weatherResponse.main.feels_like, weatherResponse.visibility* 0.00062137119, weatherResponse.main.pressure*0.02953)
+        val riseTime = Date(weatherResponse.sys.sunrise.toLong() * 1000)
+        var sunrise = SimpleDateFormat("hh:mm a").format(riseTime)
+        val setTime = Date(weatherResponse.sys.sunset.toLong() * 1000)
+        var sunset = SimpleDateFormat("hh:mm a").format(setTime)
+        binding.sunDataTv.text = getString(R.string.sun_data, sunrise, sunset)
     }
 
     private fun displayUpdateFailed() {
